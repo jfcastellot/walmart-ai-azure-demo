22a800b13402a5d3c087da21e623419fe3b46c49
Jose Felipe Castellot Del Razo <dandrak@hotmail.com>
2026-05-04 12:46:18 -0600

    Bloque 9: optimización de imports en indexer.py

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -11,7 +11,6 @@ from azure.search.documents import SearchClient
 from azure.search.documents.indexes import SearchIndexClient
 from azure.search.documents.indexes.models import (
     SearchIndex,
-    SearchField,
     SearchFieldDataType,
     SimpleField,
     SearchableField,
